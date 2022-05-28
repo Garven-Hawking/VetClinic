3ca1e3d5833a9c1eb5d61aa09094bde154fc75d2
Garven <khuramshin2001@gmail.com>
2022-05-28 13:53:42 +0300

    main page almost ready

---
+++ src/main.js
@@ -0,0 +1,10 @@
+$(function (){
+
+    $('.team_slider').slick({
+        //dots: true,
+        infinite: false,
+        arrows:true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+    });
+})
